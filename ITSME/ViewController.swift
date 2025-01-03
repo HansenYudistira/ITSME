0243add84359e5455536b6c884477334cbe56bf9
HansenYudistira <126214334+HansenYudistira@users.noreply.github.com>
2025-01-03 18:48:32 +0700

    Initial Commit

---
+++ ITSME/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITSME
+//
+//  Created by Hansen Yudistira on 03/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
